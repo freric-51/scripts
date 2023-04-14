35442439c50fd64f5fce13bae71063756ed18fc6
freric-51 <ricdefreitas@hotmail.com>
2023-04-14 14:44:28 -0300

    syncs

--- backup_bin.sh
+++ backup_bin.sh
@@ -26,15 +26,15 @@ function ifs_restore {
 }
 
 function exe_backup {
-	echo -e "${COLOR_YELLLOW}-$1 x $2-${COLOR_RESET}"
+    echo -e "${COLOR_YELLLOW}-$1 x $2-${COLOR_RESET}"
 
-	if ! [ -d "$1" ]; then
-		mkdir "$1"
-	fi
+    if ! [ -d "$1" ]; then
+        mkdir "$1"
+    fi
 
-	if ! [ -d "$2" ]; then
-		mkdir "$2"
-	fi
+    if ! [ -d "$2" ]; then
+        mkdir "$2"
+    fi
 
     if [ -z "$3" ]; then
         ret1=$(cp --verbose --preserve --update "$1"/*.* "$2"  2>/dev/null)
@@ -44,95 +44,95 @@ function exe_backup {
         ret2=$(cp --verbose --preserve --update "$2"/$3 "$1"  2>/dev/null)
     fi
 
-	if [ "$ret1" != "" ]; then
-		echo -e "$ret1"
-	fi
-	if [ "$ret2" != "" ]; then
-		echo -e "$ret2"
-	fi
+    if [ "$ret1" != "" ]; then
+        echo -e "$ret1"
+    fi
+    if [ "$ret2" != "" ]; then
+        echo -e "$ret2"
+    fi
 }
 
 function remove_file {
-	if [ -e "$1/$3" ]; then
-		# echo -e "To trash $1/$3"
-		gio trash "$1/$3"
-	fi
-	if [ -e "$2/$3" ]; then
-		# echo -e "To trash $2/$3"
-		gio trash "$2/$3"
-	fi
+    if [ -e "$1/$3" ]; then
+        # echo -e "To trash $1/$3"
+        gio trash "$1/$3"
+    fi
+    if [ -e "$2/$3" ]; then
+        # echo -e "To trash $2/$3"
+        gio trash "$2/$3"
+    fi
 }
 
 function dropbox_isrunning {
-	#dropbox running
-	exerun=$(ps -e |  grep -i dropbox | awk '{print $1}')
-	if [ -z "$exerun" ]; then
-		exerun=0
-	fi
-	echo $exerun
+    #dropbox running
+    exerun=$(ps -e |  grep -i dropbox | awk '{print $1}')
+    if [ -z "$exerun" ]; then
+        exerun=0
+    fi
+    echo $exerun
 }
 
 function transfer_files {
-	# =========================================
-	A="/home/ric/bin"
-	B="/home/ric/Dropbox/linux/bin"
+    # =========================================
+    A="/home/ric/bin"
+    B="/home/ric/Dropbox/linux/bin"
     cd "$A"
-	# =========================================
+    # =========================================
 
-	# -------------------------------------------------------------
-	C=""
-	exe_backup "$A$C" "$B$C"
+    # -------------------------------------------------------------
+    C=""
+    exe_backup "$A$C" "$B$C"
 
-	# -------------------------------------------------------------
-	C="/App.Image"
-	exe_backup "$A$C" "$B$C"
-	remove_file "$A$C" "$B$C" "AppImageUpdate-x86_64.AppImage"
-	remove_file "$A$C" "$B$C" "avidemux_2.7.4.appImage"
-	remove_file "$A$C" "$B$C" "avidemux_2.7.6.appImage"
-	remove_file "$A$C" "$B$C" "avidemux_2.7.8.appImage"
-	remove_file "$A$C" "$B$C" "avidemux_2.8.0.appImage"
-	remove_file "$A$C" "$B$C" "ClipGrab-3.8.14-x86_64.AppImage"
-	remove_file "$A$C" "$B$C" "ClipGrab-3.9.6-x86_64.AppImage"
-	remove_file "$A$C" "$B$C" "CPU-X_v3.2.4_x86_64.AppImage"
-	remove_file "$A$C" "$B$C" "CPU-X-v4.0.1-x86_64.AppImage"
-	remove_file "$A$C" "$B$C" "Franz-5.9.2.AppImage"
-	remove_file "$A$C" "$B$C" "helio-3.1-x64.AppImage"
+    # -------------------------------------------------------------
+    C="/App.Image"
+    exe_backup "$A$C" "$B$C"
+    remove_file "$A$C" "$B$C" "AppImageUpdate-x86_64.AppImage"
+    remove_file "$A$C" "$B$C" "avidemux_2.7.4.appImage"
+    remove_file "$A$C" "$B$C" "avidemux_2.7.6.appImage"
+    remove_file "$A$C" "$B$C" "avidemux_2.7.8.appImage"
+    remove_file "$A$C" "$B$C" "avidemux_2.8.0.appImage"
+    remove_file "$A$C" "$B$C" "ClipGrab-3.8.14-x86_64.AppImage"
+    remove_file "$A$C" "$B$C" "ClipGrab-3.9.6-x86_64.AppImage"
+    remove_file "$A$C" "$B$C" "CPU-X_v3.2.4_x86_64.AppImage"
+    remove_file "$A$C" "$B$C" "CPU-X-v4.0.1-x86_64.AppImage"
+    remove_file "$A$C" "$B$C" "Franz-5.9.2.AppImage"
+    remove_file "$A$C" "$B$C" "helio-3.1-x64.AppImage"
 
-	# -------------------------------------------------------------
-	C="/Converte"
-	exe_backup "$A$C" "$B$C"
+    # -------------------------------------------------------------
+    C="/Converte"
+    exe_backup "$A$C" "$B$C"
 
-	# -------------------------------------------------------------
-	C="/filter_torrent"
-	exe_backup "$A$C" "$B$C"
+    # -------------------------------------------------------------
+    C="/filter_torrent"
+    exe_backup "$A$C" "$B$C"
 
-	# -------------------------------------------------------------
-	C="/PoucoUso.03"
-	exe_backup "$A$C" "$B$C"
+    # -------------------------------------------------------------
+    C="/PoucoUso.03"
+    exe_backup "$A$C" "$B$C"
 
-	for arq in $( find  $B$C/* -type f  -printf "%f\n" ); do
-		# echo -e ">>>	apagar $arq"
-		remove_file "$A" "$B" "$arq"
-	done
+    for arq in $( find  $B$C/* -type f  -printf "%f\n" ); do
+        # echo -e ">>>	apagar $arq"
+        remove_file "$A" "$B" "$arq"
+    done
 
-	# teste 15 mar 2023 ==========================
-	remove_file "$A" "$B" "aaaa bbbb cccc.txt"
-	remove_file "$A" "$B" "aaaa"
-	remove_file "$A" "$B" "bbbb"
-	remove_file "$A" "$B" "cccc"
-	remove_file "$A$C" "$B$C" "aaaa bbbb cccc.txt"
-	remove_file "$A$C" "$B$C" "aaaa"
-	remove_file "$A$C" "$B$C" "bbbb"
-	remove_file "$A$C" "$B$C" "cccc"
-	# teste 15 mar 2023 ==========================
+    # teste 15 mar 2023 ==========================
+    remove_file "$A" "$B" "aaaa bbbb cccc.txt"
+    remove_file "$A" "$B" "aaaa"
+    remove_file "$A" "$B" "bbbb"
+    remove_file "$A" "$B" "cccc"
+    remove_file "$A$C" "$B$C" "aaaa bbbb cccc.txt"
+    remove_file "$A$C" "$B$C" "aaaa"
+    remove_file "$A$C" "$B$C" "bbbb"
+    remove_file "$A$C" "$B$C" "cccc"
+    # teste 15 mar 2023 ==========================
 
-	remove_file "$A$C" "$B$C" "update_pips (2023 02).py"
-	remove_file "$A$C" "$B$C" "update_pips (cópia 1).py"
-	remove_file "$A$C" "$B$C" "python_todo.txt"
+    remove_file "$A$C" "$B$C" "update_pips (2023 02).py"
+    remove_file "$A$C" "$B$C" "update_pips (cópia 1).py"
+    remove_file "$A$C" "$B$C" "python_todo.txt"
 
-	# WWW server
+    # WWW server
     A="/var/www/html"
-	B="/home/ric/Dropbox/linux/apache/html"
+    B="/home/ric/Dropbox/linux/apache/html"
     if [ -d $A ]; then
         cd "$A"
         # -----------------------
@@ -151,10 +151,10 @@ function transfer_files {
 
     # GITHUB folder
     A="/home/ric/Documents/code/GitHub/scripts"
-	B="/home/ric/bin"
+    B="/home/ric/bin"
     if [ -d $A ]; then
         cd "$A"
-         # -----------------------
+        # -----------------------
         C=""
         exe_backup "$A$C" "$B$C" "backup_bin.sh"
         exe_backup "$A$C" "$B$C" "dev_pc_list.py"
@@ -186,9 +186,9 @@ echo -e "----------------------------\nInicio... verifique manualmente arquivos 
 ifs_set		# to accept space in file names
 primeira_vez=1
 while
-	dropbox_stop
-	transfer_files
-	dropbox_start
+    dropbox_stop
+    transfer_files
+    dropbox_start
 	if [ ! -z "$1" ] ; then
 		t=$(($RANDOM % 19 + 4))
         if [ $primeira_vez == 1 ] ; then
