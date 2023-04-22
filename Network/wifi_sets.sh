2ee1ab3a78e1cf5f3fe16fe2be5e418a7f50bc19
freric-51 <ricdefreitas@hotmail.com>
2023-04-22 15:22:03 -0300

    Mudanças a serem submetidas: 	modified:   Network/wifi_sets.sh

--- Network/wifi_sets.sh
+++ Network/wifi_sets.sh
@@ -1,114 +1,91 @@
 #!/usr/bin/env bash
-
-# sudo apt install iputils-arping ifupdown ifupdown-extra
+# Freitas April 22 2023
+# kernel  5.15.0-70-generic
+# keep wifi working
+# if it stops then off/on the radio to force reconnecting
 
 function find_device {
+    # wifi devices start with WL
 	cd "/sys/class/net/"
 	ls -d wl*
 }
 
 function connected {
-    V=`iw $placa link`
-    #echo $V
-    if [ $V eq "Not connected."]; then
-        V=0
+    # parameter: device name
+    # return: 1= connected, 0= not
+
+    # 4163 0x1043 = UP,BROADCAST,RUNNING,        MULTICAST
+    # 4098 0x1002 =    BROADCAST,                MULTICAST
+    # 8843 0x228b = UP,BROADCAST,RUNNING,SIMPLEX,MULTICAST
+    # NETFLAGS=`ifconfig $placa | grep -i flags | cut -f1 -d\< | cut -f2 -d=`
+    # if [ $NETFLAGS -eq 4098 ]; then
+
+    NET=`nmcli networking`
+    if [ $NET == "disabled" ]; then
+        nmcli networking on
+        sleep 2
+    fi
+
+    RADIO=`nmcli radio wifi`
+    if [ $RADIO == "disabled" ]; then
+        nmcli radio wifi on
+        sleep 20
+    fi
+
+    ESSID=`iwconfig $1 | grep -i ESSID | cut -d" " -f8 `
+    if [ $ESSID == 'ESSID:off/any' ]; then
+        # try to connect to first wifi ESSID
+        RUN=`nmcli device set $1 autoconnect yes`
+        RUN=`nmcli con show | grep wifi | cut -d" " -f1 | head -n 1 | xargs -I{}  nmcli con up id {}`
+        sleep 20
+        FUNCTIONAL=0
     else
-        V=1
-    if
-	return $?
-
-    # tentativa errada, o link fisico está ON independente da conexão.
-    # V=`cat /sys/class/net/$placa/carrier`
-
-    # tentativa errada, "Not connected." mesmo tendo ping
-    # V=`iw $placa link`
-    #echo $V
-    # "Not connected."
-
+        Quality=`iwconfig $1 | grep -i 'link quality' | awk '{ print $2 }' | cut -d"=" -f2 | cut -d"/" -f1`
+        if [ $Quality -lt 20 ]; then
+            FUNCTIONAL=0
+        else
+            # 1 pacotes transmitidos, 0 recebidos, 100% perda de pacote, tempo 0ms
+            # 1 packets transmitted, 1 received, 0% packet loss, time 0ms
+            # RECEbido ~ RECEived
+            Received=`ping -c1 -q -W10 1.1.1.1 | sed -r '/^[\s\t]*$/d' | grep -i "rece" | cut -f2 -d, | cut -d" " -f2`
+            if [ $Received -eq 0 ]; then FUNCTIONAL=0; else FUNCTIONAL=1; fi
+        fi
+    fi
+    echo $FUNCTIONAL
 }
 
-function set_wifi_network {
-	sudo ifconfig $placa down
-	# sudo iw reg set GY
-	sleep 2
-	sudo ifconfig $placa up
-	sleep 2
-	sudo iwconfig $placa txpower on
-	# iw reg get
-	sleep 1
-	return 2
-}
+# =======
+#  MAIN
+# =======
 
-placa=`find_device`
+placa=$(find_device)
+echo -e "Monitoração wifi de $placa"
 
-echo -e "configuracao wifi de $placa.\n"
-cnn=`connected`
-echo $cnn
-cnn=`set_wifi_network`
-echo $cnn
-
-SAI_n=2
+SAI_n=999
 while [ 1 -ne $SAI_n ]; do
-	cnn=`connected`
+
+	cnn=$(connected $placa)
 
 	if [ $cnn -eq "1" ]; then
-		echo -e "$cnn online"
-		exit
+		echo -e "$cnn online - `date +%H:%M:%S`"
 	else
-		echo -e "$cnn offline"
+		echo -e "$cnn offline - `date +%H:%M:%S`"
+        sleep 0.1
+        echo -e "\tstopping wifi ..."
+        # sudo service network-manager stop
+        nmcli radio wifi off
+        sleep 5
+        echo -e "\tstarting wifi ..."
+        # sudo service network-manager start
+        nmcli radio wifi on
 	fi
-
-	sudo sleep 0.1
-	echo -e "stopping wifi ...\n"
-	sudo service network-manager stop
-	sleep 5
-	echo -e "starting wifi ...\n"
-	sudo service network-manager start
-	sleep 90
-
-	cnn=`connected`
-
-	if [ $cnn -eq "1" ]; then
-		echo -e "$cnn online"
-		exit
-	else
-		echo -e "$cnn offline"
-	fi
-
-	echo -e "reloading wifi ...\n"
-	sudo service network-manager reload
 
 	trap $SAI_n=1 SIGHUP SIGINT SIGTERM
-	if [ 1 -ne $SAI_n ]; then
-		sleep 120
+	if [ 1 -eq $SAI_n ]; then
+        echo "saindo."
+	else
+    	for i in {0..4}; do
+            sleep 11
+        done
 	fi
 done
-
-exit
-
-
-
-
-
-# # sudo ifdown $placa
-# # sleep 3
-# # sudo ifup -v $placa
-# # sleep 3
-
-# #sudo iwconfig $placa retry 7  # Retry short limit:7 4F=max
-# #sudo iwconfig $placa rts 2347 # RTS thr=2347
-# #sudo iwconfig $placa frag off # Fragment thr:off
-
-# sudo iwconfig $placa retry min limit 31
-# sudo iwconfig $placa rts 7168
-# sudo iwconfig $placa frag 512
-
-# sudo iwconfig $placa essid on
-# sudo iwconfig $placa channel auto
-# sudo iwconfig $placa rate 11M
-# sudo iwconfig $placa txpower auto
-
-# #sudo iwconfig $placa modu 11b
-
-# echo -e "\n"
-# iwconfig
